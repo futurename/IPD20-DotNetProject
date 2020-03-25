d245d74c2e11ebff59ae8255125fe7fb72560d71
W <futurename@gmail.com>
2020-03-25 19:08:20 -0400

    api test

---
+++ TestAPIWpf/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace TestAPIWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //ApiHelper.InitializeClient();
+
+            //tbResult.Text = GetPrice();
+            GetPrice();
+            
+        }
+
+        public async void GetPrice()
+        {
+            //Define your baseUrl
+            string baseUrl = "https://financialmodelingprep.com/api/v3/stock/real-time-price/AAPL";
+            //Have your using statements within a try/catch block
+            try
+            {
+                //We will now define your HttpClient with your first using statement which will use a IDisposable.
+                using (HttpClient client = new HttpClient())
+                {
+                    //In the next using statement you will initiate the Get Request, use the await keyword so it will execute the using statement in order.
+                    using (HttpResponseMessage res = await client.GetAsync(baseUrl))
+                    {
+                        //Then get the content from the response in the next using statement, then within it you will get the data, and convert it to a c# object.
+                        using (HttpContent content = res.Content)
+                        {
+                            //Now assign your content to your data variable, by converting into a string using the await keyword.
+                            var data = await content.ReadAsStringAsync();
+                            //If the data isn't null return log convert the data using newtonsoft JObject Parse class method on the data.
+                            if (data != null)
+                            {
+                                //Now log your data in the console
+                                MessageBox.Show(data);
+                                var quote = JsonConvert.DeserializeObject<Quote>(data);
+                                MessageBox.Show(quote.ToString());
+                                
+                            }
+                            else
+                            {
+                                Console.WriteLine("NO Data----------");
+                               
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Exception Hit------------");
+                Console.WriteLine(exception);
+            }
+        }
+       
+    }
+
+    public class Quote
+    {
+        public string Symbol { get; set; }
+        public double Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Symbol}, {Price}";
+        }
+    }
+}
