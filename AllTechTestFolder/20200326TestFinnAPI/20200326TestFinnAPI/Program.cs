36308e1aa6531744a3aa4d8942c6be8c6537460f
W <futurename@gmail.com>
2020-03-26 23:16:45 -0400

    Test accessing API and parse JSON data

---
+++ AllTechTestFolder/20200326TestFinnAPI/20200326TestFinnAPI/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ThreeFourteen.Finnhub.Client;
+
+namespace _20200326TestFinnAPI
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+           // Console.Out.WriteLine(JValue.Parse(GetInfo().Result).ToString(Formatting.Indented));
+
+           Console.Out.WriteLine(JValue.Parse(GetWithHttpClient().Result).ToString(Formatting.Indented));
+            Console.ReadKey();
+        }
+
+        public static async Task<string> GetInfo()
+        {
+            var client = new FinnhubClient("bpuhd3nrh5rbbhoij1og");
+            var rawData = await client.GetRawDataAsync("stock/profile", new Field(FieldKeys.Symbol, "AAPL"));
+            //Console.Out.WriteLine("Result: \n"+ rawData.ToString() );
+            return rawData;
+
+        }
+
+        public static async Task<string> GetWithHttpClient()
+        {
+            const string baseUrlFinn = "https://finnhub.io/api/v1/stock/metric?symbol=AAPL&metric=margin&token=bpuhd3nrh5rbbhoij1og";
+            const string baseUrlFMP = "https://financialmodelingprep.com/api/v3/historical-price-full/AAPL";
+            const string filepath =
+                "C:\\Users\\WW\\Desktop\\SourceTree\\IPD20_DotNet_WW\\IPD20-DotNetProject\\AllTechTestFolder\\20200326TestFinnAPI\\FMP.txt";
+            HttpClient client = new HttpClient();
+            try
+            {
+                HttpResponseMessage response = await
+                    client.GetAsync(baseUrlFMP);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                File.WriteAllText(filepath, responseBody);
+
+                return responseBody;
+            }
+            catch (Exception ex)
+            {
+                throw new SystemException(ex.Message);
+            }
+
+        }
+    }
+}
--- AllTechTestFolder/TestAPIWpf/MainWindow.xaml.cs
+++ AllTechTestFolder/TestAPIWpf/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Media;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -14,7 +17,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ThreeFourteen.Finnhub.Client;
 
 namespace TestAPIWpf
 {
@@ -23,23 +29,48 @@ namespace TestAPIWpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static string filePath;
+
         public MainWindow()
         {
             InitializeComponent();
             //ApiHelper.InitializeClient();
 
             //tbResult.Text = GetPrice();
-            GetPrice();
+
+
+            //GetPrice();
             
+           // tbResult.Text = GetPriceFromFinnhub().Result;
+            GetPriceFromFinnhub();
+
+
         }
+
+        public static async void GetPriceFromFinnhub()
+        {
+            var client = new FinnhubClient("bpuhd3nrh5rbbhoij1og");
+            //var rawData = await client.GetRawDataAsync("/quote", new Field(FieldKeys.Symbol, "AAPL"));
+            //string result = JValue.Parse(rawData.ToString()).ToString(Formatting.Indented);
+            //MessageBox.Show(result);
+            string result = await new HttpClient().GetStringAsync("http://news.sina.com.cn");
+            MessageBox.Show(result);
+            // return result;
+        }
+
 
         public async void GetPrice()
         {
             //Define your baseUrl
-            string baseUrl = "https://financialmodelingprep.com/api/v3/stock/real-time-price/AAPL";
+            //string baseUrl = "https://financialmodelingprep.com/api/v3/stock/real-time-price/AAPL";
+           // string baseUrl =
+            //    " https://cloud.iexapis.com/stable/stock/AAPL/financials/2?token=pk_777ee770a8f0411a88daa91ccd90b52c ";
+
+            string baseUrl = "https://finnhub.io/docs/api/quote?symbol=AAPL?token=bpuhd3nrh5rbbhoij1og";
             //Have your using statements within a try/catch block
             try
             {
+
                 //We will now define your HttpClient with your first using statement which will use a IDisposable.
                 using (HttpClient client = new HttpClient())
                 {
@@ -55,15 +86,24 @@ namespace TestAPIWpf
                             if (data != null)
                             {
                                 //Now log your data in the console
-                                MessageBox.Show(data);
-                                var quote = JsonConvert.DeserializeObject<Quote>(data);
-                                MessageBox.Show(quote.ToString());
-                                
+                                // MessageBox.Show(data);
+                                //var quote = JsonConvert.DeserializeObject<Quote>(data);
+                                //MessageBox.Show(quote.ToString());
+
+                                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                                saveFileDialog.Filter = "Txt | *.txt";
+                                if (saveFileDialog.ShowDialog() == true)
+                                {
+                                    filePath = saveFileDialog.FileName;
+                                    File.WriteAllText(filePath, data);
+
+                                }
+
                             }
                             else
                             {
                                 Console.WriteLine("NO Data----------");
-                               
+
                             }
                         }
                     }
@@ -74,9 +114,24 @@ namespace TestAPIWpf
                 Console.WriteLine("Exception Hit------------");
                 Console.WriteLine(exception);
             }
+
+
         }
-       
+
+
+        private void BtLoad_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Txt |*.txt";
+            if (fileDialog.ShowDialog() == true)
+            {
+                string result = File.ReadAllText(fileDialog.FileName);
+                string formattedString = JValue.Parse(result).ToString(Formatting.Indented);
+                tbResult.Text = formattedString;
+            }
+        }
     }
+
 
     public class Quote
     {
