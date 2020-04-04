af6f8183e3c4acefbe633bd38cb56281ba6b315d
W <futurename@gmail.com>
2020-04-04 16:19:25 -0400

    Adjust layout of market tab

--- StockMonitor/GUI/SearchStockUserControl.xaml.cs
+++ StockMonitor/GUI/SearchStockUserControl.xaml.cs
@@ -67,6 +67,7 @@ namespace GUI
             LoopRefreshData(mainListTask, watchlistTask);
 
 
+
         }
 
 
@@ -336,9 +337,11 @@ namespace GUI
                    this.Dispatcher.Invoke(() =>
                    {
                        lbSearchResult.ItemsSource = symbolList;
-                       
+                       lbSearchResult.Height = symbolList.Count * 25;
                        lbSearchResult.Visibility = Visibility.Visible;
+                       
                    });
+                   
                 }
                 else
                 {
